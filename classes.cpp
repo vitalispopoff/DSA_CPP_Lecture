229d1688303facf1a695c658be724372bc1b1703
vitalispopoff <vitalis.popoff@gmail.com>
2021-03-17 15:33:10 +0100

    classes examples, and excercises

--- classes.cpp
+++ classes.cpp
@@ -1,29 +1,29 @@
 #include "_externalLibs.h"
 #include "classes.h"
 
-// Examples ------------------------------------
+// Example 1 -----------------------------------
 
 class ExampleClass1 {
 
 public:
+	char c;
 
-	char c = 165;
+	ExampleClass1() {
 
-	ExampleClass1();
-	~ExampleClass1();
+		c = (char) 165;
+
+		cout << "\n\t" << "ExampleClass1 constructor successfully called." << endl;
+	}
+
+	~ExampleClass1() {
+
+		c = 2;
+	}
 };
 
-ExampleClass1::ExampleClass1() {
-
-	cout << "\n\t"<<"ExampleClass1 constructor successfully called."<< endl;
-}
-
-ExampleClass1::~ExampleClass1() {
-
-	c = 2;
-}
-
 void classesExample1() {
+
+	cout << "\n Example 1 ------------------------------------\n";
 
 	char* ref1;
 
@@ -34,11 +34,11 @@ void classesExample1() {
 
 		ref1 = &e1.c;
 
-	cout << "\n\te1.c adr :\t" << (uintptr_t) ref1 << endl;
-	cout << "\n\tref :\t" << *ref1 << endl;
+		cout << "\n\te1.c adr :\t" << (uintptr_t)ref1 << endl;
+		cout << "\n\tref :\t" << *ref1 << endl;
 
-	// disposable here - with scope conclussion the destructor will be called by default
-	//e.~ExampleClass1();	
+		// disposable here - with scope conclussion the destructor will be called by default
+		//e.~ExampleClass1();	
 	}
 
 	cout << "\n\t----------scope closed----------" << endl;
@@ -46,6 +46,66 @@ void classesExample1() {
 	cout << "\n\tref :\t" << *ref1 << endl;
 }
 
+// Example 2 -----------------------------------
+
+class ExampleClass2 {
+public:
+	int var = 0;
+	ExampleClass2();
+	~ExampleClass2();
+};
+
+ExampleClass2::ExampleClass2() {
+
+	cout << "\n\tExampleClass2 constructor successfully called." << endl;
+}
+ExampleClass2::~ExampleClass2() {
+
+	cout << "\n\tExampleClass2 destructor successfully called." << endl;
+}
+
+void classesExample2() {
+
+	cout << "\n Example 2 ------------------------------------\n";
+
+	ExampleClass2 object;
+	cout << "\n\tobject variable = " << object.var << endl;
+}
+
+// Example 2 -----------------------------------
+
+class ExampleClass3 {
+
+public:
+	int var = 0;
+
+	ExampleClass3() {
+
+		var = 0;
+
+		cout << "\n\tdefault constructor called.\n";
+	}
+	ExampleClass3(int i) {
+		
+		var = i;
+		cout << "\n\tconstructor overload called.\n";
+	}
+	
+	//~ExampleClass3();	// this destructor cause a lots of problems.
+};
+
+void classesExample3() {
+
+	cout << "\n Example 3 ------------------------------------\n";
+
+	ExampleClass3 object1;
+
+	cout << "\n\tobject1 var = " << object1.var << endl;
+
+	ExampleClass3 object2 = ExampleClass3(1);
+
+	cout << "\n\tobject2 var = " << object2.var << endl;
+}
 
 // Excercise 1 ---------------------------------
 
@@ -55,28 +115,25 @@ public:
 	int age;
 	float height, weight;
 
-	Person();
-	void toString();
+	Person() {
+
+		age = (rand() % 75) + 25;
+		height = ((rand() % 100) + 120) / 100.;
+		weight = ((rand() % 750) + 500) / 10.;
+	}
+
+	void toString() {
+
+		cout << "\n\tage\t" << age << "\tyears old,"
+			<< "\n\theight\t" << height << "\tcm tall,"
+			<< "\n\tweight\t" << weight << "\tkg of weight"
+			<< endl;
+	}
+
 };
 
-Person::Person() {
-
-
-
-	age = (rand() % 75) + 25;
-	height = ((rand() % 100) + 120) / 100.;	
-	weight = ((rand() % 750) + 500) / 10.;
-}
-
-void Person::toString() {
-
-	cout << "\n\tage\t" << age <<"\tyears old,"
-		<< "\n\theight\t" << height << "\tcm tall,"
-		<< "\n\tweight\t" << weight <<"\tkg of weight"
-		<< endl;
-}
-
 void classesExcercise1() {
+	cout << "\n Excercise 1 ----------------------------------\n";
 
 	Person p = Person();
 
@@ -92,44 +149,80 @@ public:
 	int population;
 	Person* inhabitants;
 
-	House();
-	~House();
-};
 
-House::House() {
-	population = (rand() % 5) + 1;
-	inhabitants = new Person[population];
+	House() {
+		population = (rand() % 5) + 1;
+		inhabitants = new Person[population];
 
-	cout << "\n\tpopulation : " << population << endl;
-
-	for (int i = 0 ; i < population; ++i)
-	{
-		inhabitants[i] = Person();
-		//cout << "\n\t generated for " << i << endl;
-		//inhabitants[i].toString();
+		for (int i = 0; i < population; ++i)
+			inhabitants[i] = Person();
 	}
-}
 
-House::~House() {
+	~House() {
 
-	delete[] inhabitants;
-}
+		delete[] inhabitants;
+	}
+};
 
 void classesExcercise2() {
 
+	cout << "\n Excercise 2 ----------------------------------\n";
+
 	House h = House();
 
-	cout << "\n---------------------------\n";
-
-	for (int i = 0; i < h.population; ++i) h.inhabitants[i].toString();		
-
+	for (int i = 0; i < h.population; ++i) h.inhabitants[i].toString();
 }
+
+// Excercise 3 ---------------------------------
+
+class Town {
+
+public:
+	House* building;
+
+	Town() {
+
+		building = new House[1000];
+	}
+
+	~Town() {
+
+		delete[] building;
+	}
+};
+
+void classesExcercise3() {
+
+	cout << "\n Excercise 3 ----------------------------------\n";
+
+	/* in case of a default constructor,
+	a constructor call apparently ain't a necessity
+	*/
+	Town t /*= Town()*/;
+	int townPopulation = 0;
+
+	for (int i = 0; i < 1000; ++i)
+		townPopulation += t.building[i].population;
+
+	cout << "\n\ttown population = " << townPopulation << endl;
+}
+
+// Main function -------------------------------
 
 void classes() {
 
-	//classesExample1();
+	classesExample1();
 
-	//classesExcercise1();
+	//classesExample2();
+
+	//classesExample3();
+
+
+	classesExcercise1();
 
 	classesExcercise2();
+
+	classesExcercise3();
+
+	cout << "\n ----------------------------------------------\n";
 }
--- classes.h
+++ classes.h
@@ -1,4 +1,5 @@
 
+
 void classes();
 
 /*declared and initialized in stack;
@@ -10,7 +11,22 @@ until the new variable proper initialization
 */
 void classesExample1();
 
+/*class function may have separated declarations and implementations
+in that case the implementations would be outside the class code block
+*/
 void classesExample2();
+
+/* declaring, implementing, and calling
+overloaded constructors
+
+class fuctions may be implemented
+both inside or outside class declaration blocks
+
+btw. no overloading destructors available,
+also - necessity of a destructor implementations 
+ain't that obvious as well
+*/
+void classesExample3();
 
 
 /* Excercise 1:
@@ -20,11 +36,16 @@ Write a Person class w/ defined fields:
 + weight (float).
 set their values in the constructor
 */
-void classesExcercise1();
-
+//void classesExcercise1();
 
 /* Excercise 2:
 Write a House class containig 
 a random number of people in it
 */
-void classesExcercise2();
+//void classesExcercise2();
+
+/* Excercise 3:
+Write a Town class containing
+1000 houses; use previously written classes
+*/
+//void classesExcercise3();
